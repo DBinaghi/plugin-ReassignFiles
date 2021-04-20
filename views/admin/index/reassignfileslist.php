1ab38af999bf228c1318fdb5c16820a4c1bf6007
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2021-04-20 18:43:56 +0200

    Added filter input field

--- views/admin/index/reassignfileslist.php
+++ views/admin/index/reassignfileslist.php
@@ -1,8 +1,10 @@
-<p><?php echo __('Select one or more files to be reassigned to this item.'); ?></p>
-<div class="drawer-contents">
-  <?php
-  $itemId = metadata('item', 'id');
-  echo get_view()->formSelect('reassignFilesFiles[]', null , array('multiple' => true, 'size' => 10, 'style' => 'width: 500px;'), $fileNames);
-  ?>
-  <input type="hidden" name="itemId" value="<?php echo $itemId; ?>">
+<p><?php echo __('Please select one or more files to be reassigned to the above selected Item (tip: you can use the input field to filter the list).'); ?></p>
+<div class="drawer-contents" style="background: none">
+	<?php
+		$view = get_view();
+		echo $view->formText('reassignFilesFilter', null);
+		$itemId = metadata('item', 'id');
+		echo $view->formSelect('reassignFilesFiles[]', null , array('multiple' => true, 'size' => 15, 'style' => 'width: 600px;'), $fileNames);
+	?>
+	<input type="hidden" name="itemId" value="<?php echo $itemId; ?>">
 </div>
